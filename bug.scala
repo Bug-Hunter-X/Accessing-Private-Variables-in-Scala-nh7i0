510a3008ea252c02bd05368b8039545a22e96fbf
BugHunter-X <askzenai@gmail.com>
2025-02-09 11:04:05 +0100

    Add bug.scala file

---
+++ bug.scala
@@ -0,0 +1,19 @@
+```scala
+class MyClass {
+  private var internalValue: Int = 0
+
+  def updateValue(newValue: Int): Unit = {
+    internalValue = newValue
+    println(s"Value updated to: $internalValue")
+  }
+
+  def getValue(): Int = internalValue
+}
+
+object Main extends App {
+  val myObject = new MyClass
+  myObject.updateValue(10)
+  println(s"Value from getter: "+ myObject.getValue())
+  // myObject.internalValue = 20 // This will cause a compilation error
+}
+```
